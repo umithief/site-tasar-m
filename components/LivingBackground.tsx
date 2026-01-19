39ed974ce9a019ce66e99488e05c0eb5454f5e42
Adiniz Soyadiniz <email@adresiniz.com>
2026-01-19 13:31:47 +0300

    Chore: Deep cleanup - remove unused mobile/ui components and duplicates

--- components/LivingBackground.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const LivingBackground: React.FC = () => {
-    return (
-        <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden bg-black">
-            {/* Pure Black Background - No Effects */}
-        </div>
-    );
-};
